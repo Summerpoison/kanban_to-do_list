a5b9832d1ec24b9b745a2eb9c51e31b4d8b38250
Miriam Piotrowski <miriampiotrowski2206@gmail.com>
2026-05-08 23:00:11 +0200

    added more components and basic structure

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -7,10 +7,52 @@ const Container = styled.div`
   align-items: center;
   flex-direction: column;
 `;
+const Button = styled.button`
+  display:inline-block;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-radius: 2px;
+  cursor: pointer;
+`;
+const Text = styled.input`
+  border: 2px solid #000;
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div`
+`;
+
+
 
 function App() {
   return (
-    <Container>Hellow!</Container>
+    <Container>
+      <div>
+          <h1>WIP Kanban</h1>
+          <h2>Todo List</h2>
+          <Text value={input} />
+          <Button>Add</Button>
+        <Tasks>
+          <TaskCount>
+            <b>Pending Tasks</b> 
+          </TaskCount>
+          <TaskCount>
+            <b>Completed Tasks</b>
+          </TaskCount>
+        </Tasks>
+        <div>
+          <ul>
+              /* List items consisting of tasks will be listed here */
+          </ul>
+        </div>
+        <Button>Clear</Button>
+      </div>
+    </Container>
   );
 }
 
